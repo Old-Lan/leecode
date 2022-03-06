be86b2a2bd2834b45581bb7bd4fe3084f73a0849
“Old-Lan” <whsixnine@163.com>
2022-03-06 22:38:18 +0800

    tow nums sum

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+    }
+
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        int len = nums.length;
+        for (int i = 0; i < len; i++){
+            for (int j = i+1; j < len; j++){
+                if(nums[i]+nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+}
