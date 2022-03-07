1f701d848fe9452e83507b1f7ed30d1574439756
“Old-Lan” <whsixnine@163.com>
2022-03-07 18:48:30 +0800

    goodDaysToRobBank

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello,World");
+
+        Main main = new Main();
+        int[] security = {1,2,5,4,1,0,2,4,5,3,1,2,4,3,2,4,8};
+        int time = 2;
+        System.out.println(main.goodDaysToRobBank_(security,time));
+
     }
 
 
@@ -18,5 +26,79 @@ public class Main {
             }
         }
         return null;
+    }
+
+    /**
+     * 2100. 适合打劫银行的日子(超时)
+     * @param security
+     * @param time
+     * @return
+     */
+    public List<Integer> goodDaysToRobBank(int[] security, int time) {
+        List<Integer> results = new ArrayList<>();
+        int len = security.length;
+        for (int i = time; i < len-time;i++){
+            boolean flag = false;
+            for (int j = i - time; j < i; j++){
+                if (security[j] < security[j+1]) {
+                    flag = true;
+                    break;
+                }
+            }
+            for (int k = i; k < i+time; k++){
+                if (security[k] > security[k+1]) {
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                results.add(i);
+            }
+        }
+        return results;
+    }
+
+    public List<Integer> goodDaysToRobBank_(int[] security,int time){
+        int n = security.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        for (int i = 1; i < n; i++){
+            if(security[i] <= security[i-1]){
+                left[i] = left[i-1]+1;
+            }
+            if(security[n-i-1] <= security[n-i]){
+                right[n-i-1] = right[n-i]+1;
+            }
+        }
+        List<Integer> results = new ArrayList<>();
+        for (int i = time; i < n-time; i++){
+            if(left[i] >= time && right[i] >= time){
+                results.add(i);
+            }
+        }
+        return results;
+    }
+
+
+    /**
+     * 判断数组是否非递增
+     * @param security
+     * @param begin 开始
+     * @param end 结束
+     * @return boolean
+     */
+    public boolean is_nonincrement(int[] security,int begin,int end){
+        return false;
+    }
+
+    /**
+     * 判断数组是否非递减
+     * @param security
+     * @param begin 开始
+     * @param end 结束
+     * @return boolean
+     */
+    public boolean is_nondecrease(int[] security,int begin,int end){
+        return false;
     }
 }
