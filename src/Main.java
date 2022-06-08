f090d804facbb58bc4f7e4b7df39cfc7f5a0f710
Old-Lan <whsixnine@163.com>
2022-06-08 16:16:26 +0800

    1838. 最高频元素的频数

--- src/Main.java
+++ src/Main.java
@@ -40,8 +40,15 @@ public class Main {
 //        int[] arr = {1,2};
 //        int[][] matrix = {{5,2},{1,6}};
 //        System.out.println(main.kthLargestValue(matrix, 4));
-        int[] nums = {2,-5,1,-4,3,-2};
-        System.out.println(main.maxAbsoluteSum(nums));
+//        int[] nums = {2,-5,1,-4,3,-2};
+//        System.out.println(main.maxAbsoluteSum(nums));
+        int[] nums = {9930,9923,9983,9997,9934,9952,9945,9914,9985,9982,9970,9932,9985,9902,9975,
+                9990,9922,9990,9994,9937,9996,9964,9943,9963,9911,9925,9935,9945,9933,9916,9930,
+                9938,10000,9916,9911,9959,9957,9907,9913,9916,9993,9930,9975,9924,9988,9923,9910,
+                9925,9977,9981,9927,9930,9927,9925,9923,9904,9928,9928,9986,9903,9985,9954,9938,
+                9911,9952,9974,9926,9920,9972,9983,9973,9917,9995,9973,9977,9947,9936,9975,9954,
+                9932,9964,9972,9935,9946,9966}; int k = 3056;
+        System.out.println(main.maxFrequency(nums,k));
     }
 
     public Main(){
@@ -930,6 +937,26 @@ public class Main {
             prefix_sum = prefix_sum+nums[k];
         }
         return Math.abs(prefix_sum);
+    }
+
+    /**
+     * 1838. 最高频元素的频数
+     */
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int total = 0;
+        int l = 0;
+        int max = 1;
+        for (int r = 1; r < n;r++){
+            total += (nums[r]-nums[r-1])*(r-l);
+            while (total > k && l < r){
+                total -= (nums[r]-nums[l++]);
+            }
+            max = Math.max(max, r-l+1);
+
+        }
+        return max;
     }
 
 
